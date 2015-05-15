3376ea9dcf01b18da839bcbd141d857c897eeb72
NIYIGENA Gildas <gildniy@users.noreply.github.com>
2015-05-15 22:25:37 +0200

    Update tagging.php

--- config/tagging.php
+++ config/tagging.php
@@ -2,7 +2,7 @@
 
 return array(
 	
-	// value of are passed through this before save of tags
+	// value of tags are passed through this before save of tags
 	'normalizer' => '\Conner\Tagging\TaggingUtil::slug',
 	
 	// display value of tags are passed through (for front end display)
